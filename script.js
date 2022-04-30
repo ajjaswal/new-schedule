f3b3b11f975df43454a277218e86d863cf2b4221
AJ Jaswal <jaswalakshay@yahoo.com>
2022-04-30 19:52:35 -0400

    add if statements to verify time and determine color coding

--- script.js
+++ script.js
@@ -3,4 +3,17 @@ var hour = [9, 10, 11, 12, 13, 14, 15, 16, 17];
 var saveBtn = document.querySelectorAll(".saveBtn");
 console.log(currentHour);
 
-
+document.querySelector("#currentDay").textContent = moment().format("dddd, MMMM Do YYYY");
+for (var i = 0; i < 9; i++) {
+  if (currentHour === hour[i]) {
+    console.log(i);
+    document.getElementById(i).classList.add("present");
+  }
+  if (currentHour > hour[i]) {
+    console.log(i);
+    document.getElementById(i).classList.add("past");
+  }
+  if (currentHour < hour[i]) {
+    console.log(i);
+    document.getElementById(i).classList.add("future");
+  }
